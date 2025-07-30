93266440c40104f6c51235c50a91f5f39bbefa60
Altono1 <altonobinna11@gmail.com>
2025-07-30 17:46:02 +0100

    create barchart via openpyxl

--- pivot-table.py
+++ pivot-table.py
@@ -4,8 +4,9 @@ df = pd.read_excel('supermarket_sales.xlsx')
 
 df = df[['Gender', 'Product line', 'Total']]
 
-print(df)
-
 # plotting to see the total which each gender spends in each product line
 
-df.pivot_table()
+pivot_table = df.pivot_table(index='Gender', columns='Product line', values='Total', aggfunc='sum').round(0)
+
+# export pivot table in excel format
+pivot_table.to_excel('pivot_table.xlsx', 'Report', startrow=4)
