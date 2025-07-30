4b84777ae0bfd54f3d3d22b4f6986893bebb2547
Altono1 <altonobinna11@gmail.com>
2025-07-30 18:59:03 +0100

    create executable file from pivot-to-report

--- pivot-to-report.py
+++ pivot-to-report.py
@@ -2,37 +2,42 @@ from openpyxl import load_workbook
 from openpyxl.chart import BarChart, Reference
 from openpyxl.utils import get_column_letter
 from openpyxl.styles import Font
+import os
+import sys
 
-# Putting together #2, #3, and #4 (input: pivot_table.xlsx + month , output: Report with barchart, formulas and format)
-month = 'february'
+# script for converting .py file to executable - pyinstaller --onefile pivot-to-report.py
 
-# Read workbook and select sheet
-wb = load_workbook('pivot_table.xlsx')
+application_path = os.path.dirname(sys.executable)
+
+month = input('Month Name: ')
+
+
+input_path = os.path.join(application_path, 'pivot_table.xlsx')
+wb = load_workbook(input_path)
 sheet = wb['Report']
 
-# Active rows and columns
 min_column = wb.active.min_column
 max_column = wb.active.max_column
 min_row = wb.active.min_row
 max_row = wb.active.max_row
 
-# Instantiate a barchart
+
 barchart = BarChart()
 
-# Locate data and categories
-data = Reference(sheet, min_col=min_column+1, max_col=max_column, min_row=min_row, max_row=max_row)  # including headers
-categories = Reference(sheet, min_col=min_column, max_col=min_column, min_row=min_row+1, max_row=max_row)  # not including headers
 
-# Adding data and categories
+data = Reference(sheet, min_col=min_column+1, max_col=max_column, min_row=min_row, max_row=max_row) 
+categories = Reference(sheet, min_col=min_column, max_col=min_column, min_row=min_row+1, max_row=max_row) 
+
+
 barchart.add_data(data, titles_from_data=True)
 barchart.set_categories(categories)
 
-# Make chart
+
 sheet.add_chart(barchart, "B12")
 barchart.title = 'Sales by Product line'
-barchart.style = 5  # choose the chart style
+barchart.style = 5  
 
-# Write multiple formulas with a for loop
+
 for i in range(min_column+1, max_column+1):  # (B, G+1)
     letter = get_column_letter(i)
     sheet[f'{letter}{max_row + 1}'] = f'=SUM({letter}{min_row + 1}:{letter}{max_row})'
@@ -47,4 +52,6 @@ sheet['A2'] = month
 sheet['A1'].font = Font('Arial', bold=True, size=20)
 sheet['A2'].font = Font('Arial', bold=True, size=10)
 
-wb.save(f'report_{month}.xlsx')
+output_path = os.path.join(application_path, f'report_{month}.xlsx')
+
+wb.save(output_path)
